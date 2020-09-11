cafe1c9f368b44e7ff7e13f7a972ecc0b6420b86
Abdo Tareq <abdotarekfathy@gmail.com>
2020-09-11 14:24:53 +0200

    feat: Add Auth screen design

---
+++ lib/widgets/auth/auth_form.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class AuthForm extends StatefulWidget {
+  @override
+  _AuthFormState createState() => _AuthFormState();
+}
+
+class _AuthFormState extends State<AuthForm> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Card(
+        margin: EdgeInsets.all(12),
+        child: Padding(
+          padding: const EdgeInsets.all(16),
+          child: Form(
+              child: Column(
+            // this to make column take min height like wrap content 
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              TextField(
+                keyboardType: TextInputType.emailAddress,
+                decoration: InputDecoration(labelText: 'Mail'),
+              ),
+              TextField(
+                decoration: InputDecoration(labelText: 'Username'),
+              ),
+              TextField(
+                decoration: InputDecoration(labelText: 'Password'),
+                obscureText: true,
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              RaisedButton(
+                onPressed: () {},
+                child: Text('Login'),
+              ),
+              FlatButton(onPressed: () {}, child: Text('Create new account')),
+            ],
+          )),
+        ),
+      ),
+    );
+  }
+}
