f725457e901e63feff0824af1bab3e3c3a3782fb
Abdo Tareq <abdotarekfathy@gmail.com>
2020-09-12 13:12:52 +0200

    feat: Add auto sigin & add sign out

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_chat_app/screens/auth_screen.dart';
@@ -15,6 +16,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.pink,
         backgroundColor: Colors.pink,
@@ -23,14 +25,24 @@ class MyApp extends StatelessWidget {
         buttonTheme: ButtonTheme.of(context).copyWith(
           buttonColor: Colors.pink,
           textTheme: ButtonTextTheme.primary,
-          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
         ),
         // This makes the visual density adapt to the platform that you run
         // the app on. For desktop platforms, the controls will be smaller and
         // closer together (more dense) than on mobile platforms.
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: AuthScreen(),
+      // this will auto sign user in & out managed by firebas eauth
+      home: StreamBuilder(
+        stream: FirebaseAuth.instance.userChanges(),
+        builder: (context, userSnapshot) {
+          if (userSnapshot.hasData) {
+            return ChatScreen();
+          }
+          return AuthScreen();
+        },
+      ),
     );
   }
 }
--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,4 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
@@ -10,41 +11,56 @@ class ChatScreen extends StatelessWidget {
         appBar: AppBar(
           title: Text('Title'),
           actions: [
-            IconButton(
-              onPressed: () {},
-              icon: Icon(Icons.add),
-            ),
+            DropdownButton(
+                icon: Icon(Icons.more_vert,color: Theme.of(context).primaryIconTheme.color,),
+                items: [
+                  DropdownMenuItem(
+                    value: 'logout',
+                    child: Row(
+                      children: [
+                        Icon(Icons.exit_to_app),
+                        SizedBox(
+                          width: 10,
+                        ),
+                        Text('Logout')
+                      ],
+                    ),
+                  )
+                ],
+                onChanged: (itemIdentifier) {
+                  if (itemIdentifier == 'logout') {
+                    FirebaseAuth.instance.signOut();
+                  }
+                })
           ],
         ),
         body: StreamBuilder(
-                stream: FirebaseFirestore.instance
-                    .collection('chats/GSqttGEYaWLoDMm00AQL/messages')
-                    .snapshots(),
-                builder: (context, snapshot) {
-                  if (snapshot.connectionState == ConnectionState.waiting) {
-                    return Center(
-                      child: CircularProgressIndicator(),
-                    );
-                  }
-                  final List<QueryDocumentSnapshot> docs =
-                      snapshot.data.documents;
-                  return ListView.builder(
-                    itemCount: docs.length,
-                    itemBuilder: (context, index) {
-                      return Container(
-                        child: Text(docs[index].data()['text'].toString()),
-                      );
-                    },
-                  );
+          stream: FirebaseFirestore.instance
+              .collection('chats/GSqttGEYaWLoDMm00AQL/messages')
+              .snapshots(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+            final List<QueryDocumentSnapshot> docs = snapshot.data.documents;
+            return ListView.builder(
+              itemCount: docs.length,
+              itemBuilder: (context, index) {
+                return Container(
+                  child: Text(docs[index].data()['text'].toString()),
+                );
+              },
+            );
           },
         ),
         floatingActionButton: FloatingActionButton(
             child: Icon(Icons.add),
             onPressed: () {
               FirebaseFirestore.instance
-                  .collection('chats/GSqttGEYaWLoDMm00AQL/messages').add({
-                    'text': 'added from flutter'
-                  });
+                  .collection('chats/GSqttGEYaWLoDMm00AQL/messages')
+                  .add({'text': 'added from flutter'});
             }));
   }
 
